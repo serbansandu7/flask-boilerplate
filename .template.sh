7b7f5cd02c86916d56badc6ec4eb27334bdedd74
mihaiobytex <50319512+mihaiobytex@users.noreply.github.com>
2021-03-18 15:51:08 +0200

    Delete .template.sh

--- .template.sh
+++
@@ -1,8 +0,0 @@
-export DB_HOST=127.0.0.1
-export DB_PORT=5432
-export DB_USER=postgres
-export DB_PASSWORD=postgres
-export DB_NAME=uvloop
-
-export LICENSING_PORT=8888
-export LICENSING_HOST=0.0.0.0
